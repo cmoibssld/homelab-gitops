caedd2d2ca384c5c1806109d64acfc1be586a4f3
jbosslady <jblepany@outlook.fr>
2026-07-18 12:21:31 +0200

    Azure: change VM specs to find available version. x64 VM with 2vCPU and 4Gi RAM

--- terraform/azure/vm.tf
+++ terraform/azure/vm.tf
@@ -1,13 +1,13 @@
 resource "azurerm_linux_virtual_machine" "focalboard" {
-  name                  = "myVM"
+  name                  = "homelab"
   location              = azurerm_resource_group.homelab.location
   resource_group_name   = azurerm_resource_group.homelab.name
   network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
-  size                  = "Standard_D2ads_v7"
+  size                  = "Standard_D2als_v7"
 
   priority        = "Spot"
   eviction_policy = "Deallocate"
-  max_bid_price   = -1
+  # max_bid_price   = -1
 
   zone = "3"
 
@@ -41,5 +41,5 @@ data "azurerm_platform_image" "homelab" {
   location = azurerm_resource_group.homelab.location
   publisher = "Debian"
   offer = "debian-13"
-  sku = "13"
+  sku = "13-gen2"
 } # will look up the marketplace and fetch the most recent versionI can use. Should be x64
