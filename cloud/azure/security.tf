8f2aa6a58ba1260eb30d77bf29c2c01bfc35929a
jbosslady <jblepany@outlook.fr>
2026-07-19 15:36:05 +0200

    Open port for Kubernetes connection to remotly install app on VM

--- cloud/azure/security.tf
+++ cloud/azure/security.tf
@@ -38,6 +38,18 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     source_address_prefix = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name = "Kubernetes"
+    priority = 1004
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "6443"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface_security_group_association" "homelab" {
